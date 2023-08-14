3cacbdb39962de238b5b42be989e932571b697e4
OficinaFios <142247129+OficinaFios@users.noreply.github.com>
2023-08-14 15:20:29 -0300

    Versión estable.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", async function () {
-    const direccionInput = document.getElementById("direccionInput");
-    const sugerenciasDiv = document.getElementById("sugerencias");
+    const direccionSelect = document.getElementById("direccion");
     const consultarButton = document.getElementById("consultar");
     const resultadoDiv = document.getElementById("resultado");
 
-    const spreadsheetId = '1B54exoYwmwj2hVP0Dg8-jp-UY0XS4v2e0dZVZYbkuS4';
-    const range = 'Registros!A2:A';
+    const spreadsheetId = '1B54exoYwmwj2hVP0Dg8-jp-UY0XS4v2e0dZVZYbkuS4'; // ID de tu hoja de cálculo
+    const range = 'Registros!A2:A'; // Rango que incluye las direcciones (excluyendo la primera fila con encabezados)
     const apiKey = 'AIzaSyCVzYHipbDSB-SfFXkfZbcivcPMj-ABw14';
 
     try {
@@ -13,35 +12,16 @@ document.addEventListener("DOMContentLoaded", async function () {
         const response = await fetch(url);
         const data = await response.json();
 
-        const direcciones = data.values.flat();
+        const direcciones = data.values.flat(); // Convierte el array de arrays en un array plano
 
-        direccionInput.addEventListener("input", function () {
-            const inputValue = direccionInput.value.toLowerCase();
-            const suggestions = direcciones.filter(direccion =>
-                direccion.toLowerCase().includes(inputValue)
-            );
-
-            sugerenciasDiv.innerHTML = ""; // Limpiar sugerencias anteriores
-
-            suggestions.forEach(suggestion => {
-                const suggestionDiv = document.createElement("div");
-                suggestionDiv.textContent = suggestion;
-                suggestionDiv.addEventListener("click", function () {
-                    direccionInput.value = suggestion;
-                    sugerenciasDiv.innerHTML = ""; // Limpiar sugerencias al seleccionar
-                });
-                sugerenciasDiv.appendChild(suggestionDiv);
-            });
-        });
-
-        direccionInput.addEventListener("blur", function () {
-            setTimeout(() => {
-                sugerenciasDiv.innerHTML = "";
-            }, 300);
+        direcciones.forEach(direccion => {
+            const option = document.createElement("option");
+            option.textContent = direccion;
+            direccionSelect.appendChild(option);
         });
 
         consultarButton.addEventListener("click", async function () {
-            const selectedDireccion = direccionInput.value;
+            const selectedDireccion = direccionSelect.value;
 
             const consultaRange = 'Registros!A:D'; // Cambia el rango según tus necesidades
             const consultaUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${consultaRange}?key=${apiKey}`;
